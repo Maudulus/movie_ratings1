82d9c4c795e7e31519c6aded0ae336c8c9ae766c
Adam Sheehan <a.t.sheehan@gmail.com>
2013-10-03 10:20:46 -0400

    Add actors table.

---
+++ app/models/actor.rb
@@ -0,0 +1,3 @@
+class Actor < ActiveRecord::Base
+  validates_presence_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003135943) do
+ActiveRecord::Schema.define(version: 20131003141907) do
+
+  create_table "actors", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "movies", force: true do |t|
     t.string   "title",      null: false
