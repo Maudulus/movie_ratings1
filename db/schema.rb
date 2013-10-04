5be04c8770defed319ea2605e7b11bc6f83af789
Adam Sheehan <a.t.sheehan@gmail.com>
2013-10-04 15:03:29 -0400

    Add genres.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003142104) do
+ActiveRecord::Schema.define(version: 20131003143440) do
 
   create_table "actors", force: true do |t|
     t.string   "name",       null: false
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20131003142104) do
   create_table "cast_members", force: true do |t|
     t.integer  "movie_id",   null: false
     t.integer  "actor_id",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: true do |t|
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
